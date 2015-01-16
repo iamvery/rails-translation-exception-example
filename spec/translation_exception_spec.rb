c0a8ac5083ad07e9411a46c12e9a6a2e496a61b2
Jay Hayes <ur@iamvery.com>
2015-01-16 09:34:57 -0600

    Further test behavior by rendering view with missing translation key

--- spec/translation_exception_spec.rb
+++ spec/translation_exception_spec.rb
@@ -6,4 +6,8 @@ describe 'translation exception behavior' do
   it 'raises an exception when translation key cannot be found' do
     expect { t('blerg') }.to raise_error
   end
+
+  it 'raises an exception in a view rendered with a missing translation key' do
+    expect { visit page_path }.to raise_error
+  end
 end
